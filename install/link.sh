009fc98ac97dd9a1739da9a89115d50c6fa78b9b
Emmanuel Pilande <manny@sudokrew.com>
2015-11-22 19:41:23 -1000

    Update symlink bash script
    For nvim & pianobar configs
    
    Thx https://github.com/nicknisi/dotfiles/blob/master/install/link.sh

--- install/link.sh
+++ install/link.sh
@@ -2,10 +2,30 @@
 
 DOTFILES=$HOME/.dotfiles
 
-echo "Creating symlinks"
-
-for file in **/*.symlink ; do
+echo -e "\nCreating symlinks"
+echo "=============================="
+for file in $( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' ) ; do
   target="$HOME/.$( basename $file ".symlink" )"
-  echo "creating symlink for $file"
-  ln -s $DOTFILES/$file $target
+  if [ -e $target ]; then
+    echo "~${target#$HOME} already exists... Skipping."
+  else
+    echo "Creating symlink for $file"
+    ln -s $file $target
+  fi
+done
+
+echo -e "\n\nInstalling to ~/.config"
+echo "=============================="
+if [ ! -d $HOME/.config ]; then
+  echo "Creating ~/.config"
+  mkdir -p $HOME/.config
+fi
+for config in $DOTFILES/config/* ; do
+  target=$HOME/.config/$( basename $config )
+  if [ -e $target ]; then
+    echo "~${target#$HOME} already exists... Skipping."
+  else
+    echo "Creating symlink for $config"
+    ln -s $config $target
+  fi
 done
