78c41c0ed40a6716f8797f72d6133d2e2689e0c8
Emmanuel Pilande <manny.pilande@hpe.com>
2016-08-13 00:27:58 -1000

    Add brew.sh to install CLI tools using homebrew

---
+++ install/brew.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install command-line tools using Homebrew
+
+if test ! $(which brew); then
+  echo "Installing homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew install coreutils
+brew install moreutils
+brew install findutils
+brew install entr
+brew install wget
+brew install tree
+brew install the_silver_searcher
+brew install fzf
+brew install git
+brew install hub
+brew install imagemagick --with-webp
+brew install ffmpeg --with-libvpx
+brew install tmux
+brew install neovim/neovim/neovim
+brew install ctags
+brew install zsh
+brew install nvm
+brew install p7zip
+brew install tidy-html5
+brew install ranger
+brew install youtube-dl
+brew install pianobar
