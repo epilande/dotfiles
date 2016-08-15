f379f4afe7fb7f36bfa3eb02309dfef88ab1d8be
Emmanuel Pilande <manny.pilande@hpe.com>
2016-08-15 01:09:23 -1000

    Add Karabiner settings

---
+++ karabiner/karabiner-import.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set option.vimode_fn_hjkl 1
+/bin/echo -n .
+$cli set private.disable_command_f1 1
+/bin/echo -n .
+$cli set remap.controlL2controlL_escape 1
+/bin/echo -n .
+$cli set remap.controlR2fn 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f1 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f10 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f3_lion 1
+/bin/echo -n .
+$cli set remap.mouse_keys_mode_2 1
+/bin/echo -n .
+$cli set remap.move_mouse_cursor_fn_alone 1
+/bin/echo -n .
+$cli set remap.pointing_button4_to_command_bracket_left 1
+/bin/echo -n .
+$cli set remap.pointing_button5_to_command_bracket_right 1
+/bin/echo -n .
+$cli set remap.pointing_relative_middleclick_to_scroll 1
+/bin/echo -n .
+$cli set remap.simple_vi_mode 1
+/bin/echo -n .
+$cli set repeat.initial_wait 200
+/bin/echo -n .
+$cli set repeat.wait 33
+/bin/echo -n .
+/bin/echo
