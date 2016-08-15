2cf6bfe17220bb425b2700b16461bc53462f5525
Emmanuel Pilande <manny.pilande@hpe.com>
2016-08-15 01:10:21 -1000

    Install more apps

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -10,11 +10,13 @@ brew cask install spectacle
 brew cask install iterm2
 brew cask install tower
 brew cask install slack
+brew cask install discord
 brew cask install karabiner
 brew cask install google-chrome
 brew cask install firefox
 brew cask install 1password
 brew cask install flux
+brew cask install bartender
 brew cask install gyazo
 brew cask install rescuetime
 brew cask install sublime-text
@@ -22,6 +24,12 @@ brew cask install atom
 brew cask install imageotim
 brew cask install vlc
 brew cask install licecap
+brew cask install caffeine
+brew cask install screenhero
+brew cask install skitch
+brew cask install sketch
+brew cask install sketch-toolbox
+brew cask install vmware-fusion
 
 # https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
