ec749f0438979222345e30054f5ccaed4d4dd325
Emmanuel Pilande <epilande@gmail.com>
2024-08-30 22:38:22 -0700

    chore(lazy): disable update notifications
    This commit modifies the lazyvim configuration in the lazy.lua file. The
    'checker' setting is updated to disable notifications when new plugin updates
    are found. This change aims to minimize distractions during development.

--- nvim/.config/nvim-lazyvim/lua/config/lazy.lua
+++ nvim/.config/nvim-lazyvim/lua/config/lazy.lua
@@ -35,7 +35,10 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = {
+    enabled = true, -- automatically check for plugin updates
+    notify = false, -- disable notification when new updates are found
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
