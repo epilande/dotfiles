e8171b91026bf540cac20bff55f4501397738c4f
Emmanuel Pilande <epilande@gmail.com>
2024-10-12 23:47:45 -0700

    feat(autocmds): add diagnostics disable for node_modules and .env
    - Added autocmd to disable diagnostics in node_modules directories.
    - Added autocmd to disable diagnostics for .env files.
    - Included descriptions for existing autocmds.

--- nvim/.config/nvim-lazyvim/lua/config/autocmds.lua
+++ nvim/.config/nvim-lazyvim/lua/config/autocmds.lua
@@ -2,22 +2,34 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Disable autoformat for markdown files
 vim.api.nvim_create_autocmd({ "FileType" }, {
+  desc = "Disable autoformat for markdown files",
   pattern = { "markdown" },
   callback = function()
     vim.b.autoformat = false
   end,
 })
 
--- Auto reload tmux config
+vim.api.nvim_create_autocmd("BufRead", {
+  desc = "Disable diagnostics in node_modules",
+  pattern = "*/node_modules/*",
+  command = "lua vim.diagnostic.disable(0)",
+})
+
+vim.api.nvim_create_autocmd("BufRead", {
+  desc = "Disable diagnostic for .env files",
+  pattern = "*.env",
+  command = "lua vim.diagnostic.disable(0)",
+})
+
 vim.api.nvim_create_autocmd("BufWritePost", {
+  desc = "Auto reload tmux config",
   pattern = { "tmux/.config/tmux/*.conf" },
   command = "!tmux source ~/.config/tmux/tmux.conf",
 })
 
--- Auto reload aerospace config
 vim.api.nvim_create_autocmd("BufWritePost", {
+  desc = "Auto reload aerospace config",
   pattern = { "aerospace.toml" },
   command = "!aerospace reload-config",
 })
