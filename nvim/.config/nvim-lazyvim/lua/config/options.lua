97217df8b622a1b223538784e2c3c6ae1be4f524
Emmanuel Pilande <epilande@gmail.com>
2024-11-17 23:24:11 -0800

    fix(nvim): Fix Neovim lag on remote servers with OSC 52
    Addressed an issue where Neovim would hang due to OSC 52 clipboard
    connection timeouts when accessed via SSH to a remote server. The fix
    involves:
    
    - Adjusted `vim.o.clipboard` to "unnamedplus".
    - Implemented a custom `paste` function for clipboard operations.
    - Configured `vim.g.clipboard` to use OSC 52 for copy and paste,
      avoiding timeout-related lag.

--- nvim/.config/nvim-lazyvim/lua/config/options.lua
+++ nvim/.config/nvim-lazyvim/lua/config/options.lua
@@ -3,9 +3,27 @@
 -- Add any additional options here
 
 vim.opt.relativenumber = true
-vim.opt.clipboard = ""
 vim.opt.conceallevel = 1
 vim.opt.showtabline = 0
-
 vim.opt.undodir = vim.fn.stdpath("cache") .. "/undo"
-vim.opt.undofile = true -- enable persistent undo
+
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
