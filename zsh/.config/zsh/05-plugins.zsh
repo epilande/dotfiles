3c37702c5824c24b4aa9bf2be6d21b6816f6f87d
Emmanuel Pilande <epilande@gmail.com>
2024-12-01 01:39:03 -0800

    fix(zsh): Fix conflicting keybinding

--- zsh/.config/zsh/05-plugins.zsh
+++ zsh/.config/zsh/05-plugins.zsh
@@ -8,6 +8,10 @@ source "$(brew --prefix)/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
 ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
 ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
 
+function zvm_after_init() {
+  zvm_bindkey viins "^R" fzf-history-widget
+}
+
 source "$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
 source "$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
