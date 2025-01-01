0eb8ec58d0b15ec97dfd09e6436c8b8228edf03e
Emmanuel Pilande <epilande@gmail.com>
2024-12-31 23:18:33 -0800

    feat(nvim): Configure blink.cmp
    Configure blink.cmp plugin with LuaSnip integration for snippet support,
    custom completion menu appearance, documentation window behavior, and
    custom keymaps for navigation and snippet expansion.

---
+++ nvim/.config/nvim-lazyvim/lua/plugins/completion.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    "saghen/blink.cmp",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      snippets = {
+        expand = function(snippet)
+          require("luasnip").lsp_expand(snippet)
+        end,
+        active = function(filter)
+          if filter and filter.direction then
+            return require("luasnip").jumpable(filter.direction)
+          end
+          return require("luasnip").in_snippet()
+        end,
+        jump = function(direction)
+          require("luasnip").jump(direction)
+        end,
+      },
+      completion = {
+        menu = {
+          draw = {
+            treesitter = { "lsp" },
+            gap = 2,
+          },
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+          border = "rounded",
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+          window = {
+            border = "rounded",
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+          },
+        },
+      },
+
+      signature = { window = { border = "single" } },
+
+      sources = {
+        default = { "lsp", "path", "luasnip", "snippets", "buffer" },
+      },
+
+      keymap = {
+        preset = "enter",
+        ["<C-y>"] = { "select_and_accept" },
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+      },
+    },
+  },
+}
