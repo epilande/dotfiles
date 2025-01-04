24327e270aa4b8fe687326e73f89d303b0da1e9f
Emmanuel Pilande <epilande@gmail.com>
2025-01-03 20:20:18 -0800

    feat(nvim): Add lua and python ftplugin settings
    Add filetype-specific settings for Lua and Python:
    - Lua: Add keymaps for executing current line (<leader>cx) and current file
      (<leader><leader>x)
    - Python: Set colorcolumn to 100 characters for better code style guidance

---
+++ nvim/.config/nvim-lazyvim/after/ftplugin/lua.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>cx", "<cmd>.lua<CR>", { desc = "Execute the current line" })
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
